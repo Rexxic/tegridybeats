d9f9039a4977c74fd7c2d1058a8e3ed4a0f79aec
Rxc <cedricseiz@gmail.com>
2022-03-26 16:34:00 +0100

    Removed Test

--- pull.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Use in the “Post-Receive URLs” section of your GitHub repo.
-
-if ($_POST['payload']) {
-    shell_exec('cd /var/www/tegridybeats && git fetch origin && git reset --hard origin/master');
-}
-
-?>Pull excecuted i guess
