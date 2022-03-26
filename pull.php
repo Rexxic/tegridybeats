47ff21276872936ac8254f6448afff4eeb2f4082
Rxc <cedricseiz@gmail.com>
2022-03-26 15:26:26 +0100

    Debug pull.php

--- pull.php
+++ pull.php
@@ -1,6 +1,9 @@
 <?php
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
 if ($_POST['payload']) {
     shell_exec("cd /var/www/tegridybeats && git reset –hard HEAD && git pull");
 }
