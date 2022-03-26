de69e0c1db0b3a8379776d4b4a2c58ce4c77fcd5
Rxc <cedricseiz@gmail.com>
2022-03-26 15:23:38 +0100

    Added readme

--- pull.php
+++ pull.php
@@ -1,9 +1,8 @@
 <?php
-
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ($_POST['payload']) {
     shell_exec("cd /var/www/tegridybeats && git reset –hard HEAD && git pull");
 }
 
-?>hi
+?>Pull excecuted i guess
